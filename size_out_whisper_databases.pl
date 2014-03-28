d450adf28440db5bc44c066093d309587a7fea48
Joel Schuweiler <jschuweiler@digium.com>
2014-03-28 10:48:14 -0500

    #2 - Can now specify check count, machine count, and whisper binary. Also added pod.

--- size_out_whisper_databases.pl
+++ size_out_whisper_databases.pl
@@ -3,9 +3,27 @@
 use strict;
 use warnings;
 
+use Getopt::Long;
+
 my $whisper_bin = '/usr/local/bin/whisper-create.py';
 my $junk_whisper_file = '/tmp/junk.wsp';
 my $intervals;
+my $checks = 1;
+my $machines = 1;
+my $help;
+
+GetOptions
+(
+	'help|?' => \$help,
+	"whisper_bin=s" => \$whisper_bin,
+	"checks=i" => \$checks,
+        "machines=i" => \$machines,
+) or pod2usage(2);
+
+if ($help)
+{
+	pod2usage(1);
+}
 
 foreach my $argument (@ARGV)
 {
@@ -32,7 +50,7 @@ foreach my $interval (keys(%{$intervals}))
 		
 	if (-e $junk_whisper_file)
 	{
-		print "$interval " . human_size(size => -s $junk_whisper_file) . "\n";
+		print "$interval " . human_size(size => (-s $junk_whisper_file) * $checks * $machines) . "\n";
 		unlink($junk_whisper_file);
 	}
 }
@@ -75,3 +93,41 @@ sub human_size
 	++$n and $size /= 1024 until $size < 1024;
 	return sprintf "%.2f %s", $size, ( qw[ bytes KB MB GB ] )[ $n ];
 }
+
+__END__
+=head1 NAME
+size_out_whisper_database.pl - Get the size of various retention policies
+
+=head1 SYNOPSIS
+
+size_out_whisper_database.pl [options] retention_policy
+
+  Options:
+    --help              Brief help message
+    --checks            Number of checks on a machine
+    --machines          Number of machines the policy applies to
+    --whisper_bin       Specify whisper-checks binary path
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--help>
+
+Prints a brief help message and exits
+
+=item B<--checks>
+
+The number of checks you're running on a single machine
+
+=item B<--machines> 
+
+The number of machines the specified retention policy applies to
+
+=item B<--whisper_bin>
+
+Specify the location of your whisper-checks binary
+
+=back
+
+=cut
